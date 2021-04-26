016c28ef7bf7a290b80644fc76b8a474817a6620
MazCodes <MazCodes@users.noreply.github.com>
2021-04-26 21:41:32 +0400

    Variables.tf file for VMware credentials

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "vsphere_user" {
+  description = "vcenter username"
+  type        = string
+}
+
+variable "vsphere_password" {
+  description = "vcenter password"
+  type        = string
+}
+
+variable "vsphere_server" {
+  description = "vcenter IP address"
+  type        = string
+}
